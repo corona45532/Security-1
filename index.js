181ac2d7b64f0b20342a31c392775b0922fbec02
corona45532 <83106035+corona45532@users.noreply.github.com>
2021-05-23 16:50:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ console.log('Made by FelipeSazz with Javascript and Love.\n\n')
 const client = new Ritsu(process.env.TOKEN, {
   prefix: process.env.BOT_PREFIX,
 })
-client.start()
+client.start("ODQyMjA1NDUwODI0MzE5MDM3.YJx68g.Tehw3b-zbpp2ATTIxSGP5Dc3_VQ")
